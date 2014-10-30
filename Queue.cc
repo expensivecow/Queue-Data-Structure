e8291b9aedffcd03eea95262dfb3ef8f2ff6f15b
expensivecow <expensivecow@gmail.com>
2014-10-30 11:38:17 -0700

    adding skeleton for queue code

---
+++ Queue.cc
@@ -0,0 +1,58 @@
+/* Author(s): Michael Cao, Jason Too
+ * Goal: Implement a queue using c++11 which suits the standards from scratch
+ * Date: October 30, 2014
+ */
+
+#include <iostream>
+#include "Queue.h"
+
+myQueue::myQueue(int startingTop, int startingLength) {
+  //TODO: implement
+  topIndex = startingTop;
+  maxSize = startingLength;
+  p = new int[startingLength];
+  return;
+}
+
+bool myQueue::empty() {
+  //TODO: implement
+  return false;
+}
+
+int myQueue::size() {
+  //TODO: implement
+  return 0;
+}
+
+void myQueue::push(int pushVal) {
+  //TODO: implement
+  return;
+}
+
+int myQueue::pop() {
+  //TODO: implement
+  return 0;
+}
+
+void myQueue::reSize(int capacity) {
+  //TODO: implement
+  return;
+}
+
+void myQueue::displayStack() {
+  //TODO: implement
+  return;
+}
+
+int myQueue::getMaxSize() {
+  //TODO: implement
+  return 0;
+}
+
+int* myQueue::returnArr() {
+  //TODO: implement
+  return p;
+}
+
+
+
